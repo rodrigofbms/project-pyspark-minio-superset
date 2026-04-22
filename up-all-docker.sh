b1df5c5bd38ad47a55016338597e8f4b974e7374
rodrigofbms <rodrigofbms@gmail.com>
2026-04-22 19:26:34 +0000

    Adding link from services via https and allow permission for jupyter notebook

--- up-all-docker.sh
+++ up-all-docker.sh
@@ -2,15 +2,16 @@
 
 # List of services directories
 SERVICES=(
+    "/home/rodri/docker/project-pyspark-minio-superset/apps/nginx_proxy_manager"
+    "/home/rodri/docker/project-pyspark-minio-superset/apps/portainer"
+    "/home/rodri/docker/project-pyspark-minio-superset/documentation"
+    "/home/rodri/docker/project-pyspark-minio-superset/apps/postgres_adventureworks"
     "/home/rodri/docker/project-pyspark-minio-superset/apps/airflow"
     "/home/rodri/docker/project-pyspark-minio-superset/apps/minio"
     "/home/rodri/docker/project-pyspark-minio-superset/apps/open_metadata"
     "/home/rodri/docker/project-pyspark-minio-superset/apps/spark"
     "/home/rodri/docker/project-pyspark-minio-superset/apps/superset"
     "/home/rodri/docker/project-pyspark-minio-superset/apps/trino"
-    "/home/rodri/docker/project-pyspark-minio-superset/apps/postgres_adventureworks"
-    "/home/rodri/docker/project-pyspark-minio-superset/apps/nginx_proxy_manager"
-    "/home/rodri/docker/project-pyspark-minio-superset/documentation"
 )
 
 for SERVICE in "${SERVICES[@]}"; do
