e05f7a34ca0626ad0cc7d4615ac83c9d1c15c6ab
rodrigofbms <rodrigofbms@gmail.com>
2026-04-29 00:35:42 +0000

    Fixed a permission issue in jupyter container and configuring the proxy

--- down-all-docker.sh
+++ down-all-docker.sh
@@ -5,7 +5,7 @@ SERVICES=(
     "/home/rodri/docker/project-pyspark-minio-superset/apps/airflow"
     "/home/rodri/docker/project-pyspark-minio-superset/apps/minio"
     "/home/rodri/docker/project-pyspark-minio-superset/apps/open_metadata"
-    "/home/rodri/docker/project-pyspark-minio-superset/apps/spark"
+    "/home/rodri/docker/project-pyspark-minio-superset/apps/spark_jupyter"
     "/home/rodri/docker/project-pyspark-minio-superset/apps/superset"
     "/home/rodri/docker/project-pyspark-minio-superset/apps/trino"
     "/home/rodri/docker/project-pyspark-minio-superset/apps/postgres_adventureworks"
